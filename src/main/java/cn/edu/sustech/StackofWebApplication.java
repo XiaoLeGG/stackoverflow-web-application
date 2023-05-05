fca8fae3a303f9db6d6e7747eda8d99c92a139a8
XiaoLeGG <786071433@qq.com>
2023-05-06 03:38:48 +0800

    Project build

---
+++ src/main/java/cn/edu/sustech/StackofWebApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sustech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackofWebApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StackofWebApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/cn/edu/sustech/StackofWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.sustech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackofWebApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
